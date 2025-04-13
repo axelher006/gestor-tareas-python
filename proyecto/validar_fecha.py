dbbdb21988b6431de9953bccfc7379c5d5155cda
Axel Hernandez <axel.hernandez@correo.ucu.edu.uy>
2025-04-13 17:27:32 -0300

    Agrega función validar_fecha con verificación de formato YYYY-MM-DD

---
+++ proyecto/validar_fecha.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def validar_fecha(fecha_str):
+    try:
+        datetime.strptime(fecha_str, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
